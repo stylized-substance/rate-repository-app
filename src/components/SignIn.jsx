e441fbdb156d74424fa4340e244e04f3f5c7d18b
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-04-24 23:02:31 +0300

    progress

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,53 +2,61 @@ import theme from "./theme";
 import Text from "./Text";
 import { TextInput, Pressable, View, StyleSheet } from "react-native";
 import { useFormik } from "formik";
-import * as yup from 'yup'
+import * as yup from "yup";
 import useSignIn from "../hooks/useSignIn";
 
-const styles = StyleSheet.create({
-  container: {
-    flexDirection: "column",
-    gap: 20,
-    padding: 20,
-    backgroundColor: theme.colors.backgroundContainer,
-  },
-  textInput: {
-    borderWidth: 1,
-    borderRadius: 5,
-    padding: 10,
-  },
-  error: {
-    borderWidth: 1,
-    borderRadius: 5,
-    borderColor: theme.colors.textError,
-    padding:10,
-  },
-  submitButton: {
-    backgroundColor: theme.colors.primary,
-    borderRadius: 3,
-    alignItems: "center",
-    padding: 10,
-  },
-});
+const SignIn = () => {
+  const styles = StyleSheet.create({
+    container: {
+      flexDirection: "column",
+      gap: 20,
+      padding: 20,
+      backgroundColor: theme.colors.backgroundContainer,
+    },
+    textInput: {
+      borderWidth: 1,
+      borderRadius: 5,
+      padding: 10,
+    },
+    error: {
+      borderWidth: 1,
+      borderRadius: 5,
+      borderColor: theme.colors.textError,
+      padding: 10,
+    },
+    submitButton: {
+      backgroundColor: theme.colors.primary,
+      borderRadius: 3,
+      alignItems: "center",
+      padding: 10,
+    },
+  });
 
-const validationSchema = yup.object().shape({
-  username: yup
-    .string()
-    .required('Username is required'),
-  password: yup
-    .string()
-    .required('Password is required')
-})
+  const validationSchema = yup.object().shape({
+    username: yup.string().required("Username is required"),
+    password: yup.string().required("Password is required"),
+  });
+  
+  const [signIn] = useSignIn()
 
-const SignInForm = ({ onSubmit }) => {
+  const onSubmit = async (values) => {
+    try {
+      const data = await signIn(values)
+      console.log(data)
+    } catch (error) {
+      console.log(error)
+    }
+  };
+  
   const formik = useFormik({
     initialValues: {
       username: "",
       password: "",
     },
     validationSchema,
-    onSubmit,
+    onSubmit
   });
+  
 
   return (
     <>
@@ -60,7 +68,9 @@ const SignInForm = ({ onSubmit }) => {
           onChangeText={formik.handleChange("username")}
         />
         {formik.touched.username && formik.errors.username && (
-          <Text color="error" fontSize="subheading">{formik.errors.username}</Text>
+          <Text color="error" fontSize="subheading">
+            {formik.errors.username}
+          </Text>
         )}
         <TextInput
           style={formik.errors.password ? styles.error : styles.textInput}
@@ -70,9 +80,11 @@ const SignInForm = ({ onSubmit }) => {
           secureTextEntry
         />
         {formik.touched.password && formik.errors.password && (
-          <Text color="error" fontSize="subheading">{formik.errors.password}</Text>
+          <Text color="error" fontSize="subheading">
+            {formik.errors.password}
+          </Text>
         )}
-        <Pressable onPress={formik.handleSubmit(formik.values)}>
+        <Pressable onPress={formik.handleSubmit}>
           <View style={styles.submitButton}>
             <Text fontSize="buttonText" color="textItemLanguage">
               Sign in
@@ -83,14 +95,5 @@ const SignInForm = ({ onSubmit }) => {
     </>
   );
 };
-
-const SignIn = () => {
-  const onSubmit = ({ username, password }) => {
-    //useSignIn({ username: username, password: password})
-    console.log(username)
-  }
-
-  return <SignInForm onSubmit={onSubmit} />
-}
 
 export default SignIn;
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,10 +1,14 @@
 import { useMutation } from '@apollo/client'
 import { AUTHENTICATE } from '../graphql/mutations'
 
-const useSignIn = ({ credentials }) => {
-  const [authenticate, { data }] = useMutation(AUTHENTICATE)
-  authenticate({ variables: { credentials: credentials } })
-  //console.log(data)
+const useSignIn = () => {
+  const [mutate, result] = useMutation(AUTHENTICATE)
+
+  const signIn = async (credentials) => {
+    mutate(credentials)
+  }
+
+  return [signIn, result]
 }
 
 export default useSignIn
